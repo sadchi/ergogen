e0c27d8eae3d391e500b08de5ec52b3e9eb3dd8f
sadchi <sadchi@gmail.com>
2023-06-30 05:43:11 +0300

    Added teensy2

--- src/footprints/index.js
+++ src/footprints/index.js
@@ -11,6 +11,7 @@ module.exports = {
     omron: require('./omron'),
     pad: require('./pad'),
     promicro: require('./promicro'),
+    teensy2: require('./teensy2'),
     rgb: require('./rgb'),
     rotary: require('./rotary'),
     scrollwheel: require('./scrollwheel'),
